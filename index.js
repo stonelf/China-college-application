86d5e72082d5e3326472f6017651ad47de69cc38
emu <emu@vip.qq.com>
2020-07-18 18:55:53 +0800

    增加了省份和批次列表

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var provinceList = ["内蒙古","辽宁","吉林","黑龙江","上海","江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广西","海南","重庆","四川","贵州","云南","陕西","甘肃","青海","宁夏","新疆","北京","天津","河北","山西","广东","西藏"];
+var provinceList = ["内蒙古","辽宁","吉林","黑龙江","上海","江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广西","海南","重庆","四川","贵州","云南","陕西","甘肃","青海","宁夏","新疆","北京","天津","河北","山西","广东","西藏","香港","台湾","澳门"];
 var divisionList = ["文科","理科","综合"];
-var batchList = ["本科一批","本科批","本科一批A段","平行录取一段","本科二批","本科一批B段","零志愿批次","平行录取二段","专科批","本科二批A段","本科二批B段","平行录取三段","本科三批","本科二批C段","本科三批A段","本科批A段","国家专项计划本科批","地方专项计划本科批","本科提前批","普通类提前批","专科提前批","本科综合评价批","高校专项计划本科批","本科批B段"];
-var data=[],searchBy,userID,coffeeClicked=false;
+var batchList = ["本科一批","本科批","本科一批A段","平行录取一段","本科二批","本科一批B段","零志愿批次","平行录取二段","专科批","本科二批A段","本科二批B段","平行录取三段","本科三批","本科二批C段","本科三批A段","本科批A段","国家专项计划本科批","地方专项计划本科批","本科提前批","普通类提前批","专科提前批","本科综合评价批","高校专项计划本科批","本科批B段","无批次","少数民族预科班","专科批A段","本科一批A1段"];
+
+var data=[],searchBy,userID,coffeeClicked=false,pingImg=new Image();
 var pingpath;
 function $(id){return document.getElementById(id)}
 function restore(name){
@@ -16,6 +17,8 @@ function store(name,value){
 		$("scoreBox").maxLength= searchBy=="p"?"6":"3"
 	}
 	if(searchBy=="s" && parseInt($("scoreBox").value)>750) $("scoreBox").value=750
+	if(name != "keyword") $("keywordSpan").style.display="none"
+	
 }
 function init(){
 	var e = $("provinceSelector");
@@ -39,8 +42,8 @@ function query(){
 	$("keywordSpan").style.display="none"
 	searchBy = document.forms[0].search_by.value;
 	$("spanSearchBy").innerHTML = searchBy=="s"?"分数":"位次";
-	var baseURL = "//cdn.citpu.net.cn/release/CEE";//这个域名直接从cdn透传
-//	var baseURL = "//cdn.citpu.net/release/CEE";//这个域名经过COS再透传到CDN，COS有长期cache的作用，也有首次访问的时候二次回源的缺陷。
+	var baseURL = "//stone.sou.ac.cn/release/CEE";//这个域名直接从cdn透传
+//	var baseURL = "//cdn.sou.ac.cn/release/CEE";//这个域名经过COS再透传到CDN，COS有长期cache的作用，也有首次访问的时候二次回源的缺陷。
 	var url = baseURL+"/"+$("provinceSelector").value+"/"+document.forms[0].division.value+"/"+$("percentSelector").value+"/"+$("scoreBox").value+"/"+document.forms[0].search_by.value+"/"+(coffeeClicked?"list.js":"data.js");
 	fetch(url).then(function(response) {
 		return response.json();
@@ -48,7 +51,7 @@ function query(){
 		data = d;
 		show()
 	});
-	$("ping").src=url.replace(baseURL,pingpath);
+	pingImg.src=url.replace(baseURL,pingpath);
 }
 function show(){
 	var e = $("majorList"),tr,p=$("percentSelector").value;
@@ -75,7 +78,7 @@ function filter(){
 	var e = $("majorList"),tr,p=$("percentSelector").value;
 	$("majorList").innerHTML="";
 	for(var i=0;i<data.length;i++){
-		if(r.test(JSON.stringify(data[i]))){
+		if(r.test(data[i].college+data[i].major+data[i].subMajor)){
 		tr = e.insertRow();
 		tr.insertCell().innerHTML = (data[i].college+"<wbr>"+data[i].branch).replace(r,"<font color=red>$1</font>");
 		tr.insertCell().innerHTML = (data[i].major+"<wbr>"+data[i].subMajor).replace(r,"<font color=red>$1</font>");
